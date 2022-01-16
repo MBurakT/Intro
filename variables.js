3e04b0ebed041e411f627293f82c6b1840db761e
fkymon <mbtbrk11@outlook.com>
2022-01-16 19:50:42 +0300

    BTK 2.11

--- variables.js
+++ variables.js
@@ -4,3 +4,8 @@ console.log(typeof(hamburger))
 hamburger = 10
 console.log(hamburger)
 console.log(typeof(hamburger))
+
+var aboneMi = false
+console.log(aboneMi)
+console.log(typeof(aboneMi))
+//node variables.js
