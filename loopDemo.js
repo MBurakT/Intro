ac3579a78f4347e1f60f766c22efe17b18b4452f
fkymon <mbtbrk11@outlook.com>
2022-01-23 21:31:50 +0300

    BTK 3.1

---
+++ loopDemo.js
@@ -0,0 +1,10 @@
+function sayiUret(ustLimit = 49) {
+    return Math.ceil(Math.random() * ustLimit)
+}
+
+for (var i = 0; i < 8; i++) {
+    console.log(i+'. kolon')
+    for (var j = 0; j < 6; j++) {
+        console.log(sayiUret())
+    }
+}
